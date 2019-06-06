5aef5987991694064470cdedcb353fbd6f677f2e
Nicole Parrish <nicoleparrish.parrish@gmail.com>
2019-06-06 20:43:58 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,17 +1,21 @@
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+
 end
 
 def display_card_total
-  # code #display_card_total here
-end
+ do it "accepts one argument, the card total" do
+    expect { 
+      display_card_total(7)
+    } . to_not raise_error
+  end
 
 def prompt_user
-  # code #prompt_user here
+  expect($stdout) . to
+  receive(:puts). with("Type 'h' to hit or 's' stay"
 end
 
 def get_user_input
